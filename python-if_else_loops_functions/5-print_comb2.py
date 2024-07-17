e210997c1b85064af10649949329ccc77a186dae
GregSQT <9604@holbertonstudents.com>
2024-07-17 13:09:10 +0000

    message

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 nb = 0
-while nb < 99:
-    print("{:02d}".format(nb))
+while nb < 100:
+    if nb < 99:
+        print("{:02d}".format(nb), end = ", ")
+    else:
+        print("{:02d}".format(nb))
     nb = nb + 1
