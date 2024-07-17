e30fbf2af73281789046b18cb2bd49c3cf44ab16
GregSQT <9604@holbertonstudents.com>
2024-07-17 09:44:20 +0000

    message

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import ascii
+
+print(ascii.charlist())
---
+++ python-if_else_loops_functions/s1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+  
+s = str(number)
+lastDigit = int(s[-1])
+
+if number < 0:
+    lastDigit = lastDigit * -1
+
+if lastDigit > 5:
+    print("Last digit of", number, "is", lastDigit, "and is greater than 5")
+elif lastDigit == 0:
+    print("Last digit of", number, "is", lastDigit, "and is 0")
+else:
+    print("Last digit of", number, "is", lastDigit, "and is less than 6 and not 0")
+
