74b5e654b48325dc35c5507bbc8100c941651f31
GregSQT <9604@holbertonstudents.com>
2024-07-17 12:09:55 +0000

    message

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-import ascii
 
-print(ascii.charlist())
+#import string
+#for alpha_letters in string.ascii_lowercase:
+#    if alpha_letters not in 'qe':
+#       print(alpha_letters, end="")
+
+for letter_code in range(ord('a'), ord('z')+1):
+    letter = chr(letter_code)
+    if letter not in "qe":
+        print(letter, end="")
