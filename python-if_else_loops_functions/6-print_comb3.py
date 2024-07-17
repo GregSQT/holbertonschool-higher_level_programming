70c2388ff364a9b19f444a3d5cb7e1376b382052
GregSQT <9604@holbertonstudents.com>
2024-07-17 14:17:44 +0000

    message

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,12 +3,9 @@
 for i in range(0,10):
      for k in range(1,10):
         if (i == 0 and k == 1):
-
-
-
             txt0 = "{}{}".format(i, k)
             print(txt0, end="")
-        else:
+        elif (i != k and k>i):
             txt1="{}{}".format(i, k)
             print(", ", txt1, end="")
 print ()
