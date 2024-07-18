2c9f738b1d2a37cd27fb34374b7a7ce345b81bbd
GregSQT <9604@holbertonstudents.com>
2024-07-18 08:01:23 +0000

    message

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-nb = 1
-while nb < 101:
-    if nb % 3 == 0:
-        return ("Fizz")
-    elif nb % 5 == 0:
-        return ("Buzz")
-    else:
-        return (nb)
+def fizzbuzz():
+    nb = 1
+    while nb < 101:
+        if nb % 3 == 0:
+            return ("Fizz")
+        elif nb % 5 == 0:
+            return ("Buzz")
+        else:
+            return (nb)
     nb = nb + 1
