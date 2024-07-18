25c31f546fb80323116f77a50a2b51d582b6769e
GregSQT <9604@holbertonstudents.com>
2024-07-18 12:09:52 +0000

    message

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 
 """Definition des variables d'entree"""
 def add_integer(a, b=98):
+    """Fonction d'addition"""
     if (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
     if (type(b) is not int and type(b) is not float):
