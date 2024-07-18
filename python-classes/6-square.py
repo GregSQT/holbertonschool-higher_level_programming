298ac66eb79d222ce068eecc863f7266006e7a70
GregSQT <9604@holbertonstudents.com>
2024-07-18 09:28:22 +0000

    message

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,24 +6,24 @@ class Square():
 """This is the best square class possible woaw"""
 
     def __init__(self, new_size=0, position=(0, 0)):
-    if type(new_size) is not int:
-        raise TypeError("size must be an integer")
-    if new_size < 0:
-        raise ValueError("size must be >= ")
-    self._size = new_size
+        if type(new_size) is not int:
+            raise TypeError("size must be an integer")
+        if new_size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = new_size
 
-    if type(position) is not tuple:
-        raise TypeError("position must be a tuple of 2 positive integers")
-    if len(position) is not 2:
-        raise TypeError("position must be a tuple of 2 positive integers")
-    if type(position[0]) is not int or type(position[1]) is not int:
-        raise TypeError("position must be a tuple of 2 positive integers")
-    if position[0] < 0 or position[1] < 0:
-        raise TypeError("position must be a tuple of 2 positive integers")
-    self._position = position
+        if type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(position) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self._position = position
 
     def area(self):
-    return self._size * self._size
+        return self._size * self._size
 
     @property
     def size(self):
@@ -35,7 +35,7 @@ class Square():
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-    self._size = value
+        self._size = value
 
     @property
     def position(self):
