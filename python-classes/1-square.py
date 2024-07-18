6c65032d448532bd3986c6e219481d85337bdffd
GregSQT <9604@holbertonstudents.com>
2024-07-18 09:55:30 +0000

    message

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,30 +5,10 @@
 class Square():
     """Definition de la classe Square"""
 
-    def __init__(self, new_size=0, position=(0, 0)):
+    __size = 0
+    def __init__(self, new_size=0):
         if type(new_size) is not int:
             raise TypeError("size must be an integer")
         if new_size < 0:
             raise ValueError("size must be >= 0")
         self._size = new_size
-
-    @property
-    def size(self):
-        return self._size
-
-    @size.setter
-    def size(self, value):
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self._size = value
-
-    def my_print(self):
-        if self._size == 0:
-            print()
-            return
-        for i in range(self._size):
-            for j in range(self._size):
-                print("#", end="")
-            print()
