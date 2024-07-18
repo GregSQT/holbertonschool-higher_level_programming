e1335be21554401c84fbb88ebb337df4d633771d
GregSQT <9604@holbertonstudents.com>
2024-07-18 07:22:56 +0000

    message

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,9 +4,3 @@ def print_last_digit(number):
     s = str(number)
     lastDigit = s[-1]
     print("{}".format(lastDigit))
-
-
-#print_last_digit(98)
-#print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
