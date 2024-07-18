82d7e926de4cc3f6285bf4410d0eed70158a0184
GregSQT <9604@holbertonstudents.com>
2024-07-18 08:42:43 +0000

    message

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 class square:
     type: str
-
-Square = __import__('0-square').Square
+    Square = __import__('0-square').Square
 
 my_square = Square()
 print(type(my_square))
