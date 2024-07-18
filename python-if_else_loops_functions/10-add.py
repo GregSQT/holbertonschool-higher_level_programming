de812fc909d4da48cb70d88e462b62d06f63bda5
GregSQT <9604@holbertonstudents.com>
2024-07-18 07:15:38 +0000

    message

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    result = a + b
+    print("{}".format(result))
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,3 +4,9 @@ def print_last_digit(number):
     s = str(number)
     lastDigit = s[-1]
     print("{}".format(lastDigit))
+
+
+#print_last_digit(98)
+#print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
