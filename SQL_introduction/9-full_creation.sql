e5fc9b4cd3ced11e048672c8a26af7d1b9b6b58b
GregSQT <9604@holbertonstudents.com>
2024-07-18 14:10:26 +0000

    message

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,3 @@
 -- Creation de DB
+CREATE TABLE IF NOT EXISTS second_table( id INT, name VARCHAR(256) score INT);
 INSERT INTO second_table (id INT, name VARCHAR(256), score INT) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "Georges", 8);
