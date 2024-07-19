46834f84fd9e0e0aed17fdcfd3faf8509bab3e9b
GregSQT <9604@holbertonstudents.com>
2024-07-19 09:01:46 +0000

    message

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Creation de DB
-SELECT score, COUNT (*) "number" FROM second_table GROUP BY score ORDER BY COUNT(*) DESC;
+SELECT score, COUNT(*) "number" FROM second_table GROUP BY score ORDER BY COUNT(*) DESC;
